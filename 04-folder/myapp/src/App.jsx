430804d4c260ecf854662b44a4734cf585457bf4
Sushmitha Gayathri <sushmithagayathri8@gmail.com>
2026-08-29 11:10:23 +0530

    a

--- 04-folder/myapp/src/App.jsx
+++ 04-folder/myapp/src/App.jsx
@@ -12,6 +12,36 @@ const jobOpenings=[
     pay:"$8000",
     location:"Bangalore"
   },
+  {
+    img:"https://datawider.com/wp-content/uploads/2025/10/Amazon_Logo-font.jpg",
+    company:"amazon",
+    duration:"10 days ago",
+    role:"Frontend Engineer",
+    tag1:"Remote",
+    tag2:"Senior level",
+    pay:"$15000",
+    location:"hyd"
+  },
+  {
+    img:"https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/29/41/8b/29418bae-5722-ec62-c08b-c33cb69705fe/AppIcon-1x_U007emarketing-0-7-0-85-220.png/1200x630wa.png",
+    company:"zomato",
+    duration:" 15 days ago",
+    role:"delivery boy",
+    tag1:"Remote",
+    tag2:"part time",
+    pay:"$1000",
+    location:"vijayawada"
+  },
+  {
+    img:"https://th.bing.com/th/id/OIP.LugGFdeWnec5XLf-8oLkuQHaEK?w=276&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
+    company:"Tcs",
+    duration:"1week ago",
+    role:"Frontend Engineer",
+    tag1:"Remote",
+    tag2:"Senior level",
+    pay:"$18000",
+    location:"hyd"
+  }, 
   
 ];
 
